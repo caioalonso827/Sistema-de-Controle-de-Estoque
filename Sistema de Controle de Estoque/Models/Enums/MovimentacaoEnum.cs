58f4d2c3d4c9a3ae6ab86f0329ee71c99733744c
Caio Borsato Alonso <caioca827@gmail.com>
2026-01-08 21:06:23 -0300

    Delete Sistema de Controle de Estoque directory

--- Sistema de Controle de Estoque/Models/Enums/MovimentacaoEnum.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sistema_de_Controle_de_Estoque.Models.Enums
-{
-    public enum MovimentacaoEnum
-    {
-        Entrada,Saída
-    }
-}
--- Sistema de Controle de Estoque/Models/Enums/ProdutoEnum.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sistema_de_Controle_de_Estoque.Models.Enums
-{
-    public enum ProdutoEnum
-    {
-        Celulares, Relogios, CaixadeSom, Tvs, Eletrodomesticos
-    }
-}
